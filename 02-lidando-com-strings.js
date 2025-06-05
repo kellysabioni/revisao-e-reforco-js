3bf7ad4b27e4004b62045a85870420387a923c27
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-05 11:58:11 -0300

    Cria arquivo para lidar com strings
    Exemplo de concatenação

---
+++ 02-lidando-com-strings.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const nome = "Chapolin";
+const sobrenome = "Colorado";
+let idade = 20;
+let cidade = "São Paulo";
+let estado = "SP";
+
+
+// CONCATENAÇÃO
+let mensagem1 = "Meu nome é "+nome+" "+ sobrenome +", tenho "+idade+" anos e moro em "+cidade+"/"+estado+".";
+
+// TEMPLATE STRING/LITERAL
+
+
+console.log(mensagem1)
