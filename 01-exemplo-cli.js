0e40409bdcdb3ae0c706c94d390fc5929f3cf3d5
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-05 11:45:40 -0300

    Atualiza USE STRICT

---
+++ 01-exemplo-cli.js
@@ -0,0 +1,14 @@
+// Habilitando o modo estrito da linguagem 
+'use strict';
+
+let preco = 100;
+let resultado;
+
+if (preco >= 50) {
+    resultado = "Beleza";
+} else {
+    resultado = "Que droga";
+}
+
+console.log(resultado);
+
