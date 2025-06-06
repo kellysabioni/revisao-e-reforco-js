750b2149599d569e98ed35ca2752ebe06b6be6c1
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-06 09:22:04 -0300

    Atualiza

--- 02-lidando-com-strings.js
+++ 02-lidando-com-strings.js
@@ -1,3 +1,4 @@
+// 02-lidando-com-strings.js
 'use strict';
 
 const nome = "Chapolin";
@@ -6,11 +7,23 @@ let idade = 20;
 let cidade = "São Paulo";
 let estado = "SP";
 
+/* Meu nome é Chapolin Colorado, tenho 20 anos e moro em
+São Paulo/SP. */
 
-// CONCATENAÇÃO
-let mensagem1 = "Meu nome é "+nome+" "+ sobrenome +", tenho "+idade+" anos e moro em "+cidade+"/"+estado+".";
+// CONCATENAÇÃO tradicional usa o operador +
+//let mensagem1 = "Meu nome é "+nome+" "+sobrenome+", tenho "+idade+" anos e moro em "+cidade+"/"+estado+".";
+
+// Usando operador += para concatenar "por pedaços/partes"
+let mensagem1 = "Meu nome é "+nome;
+mensagem1 += " "+sobrenome+", tenho "+idade;
+mensagem1 += " anos e moro em "+cidade;
+mensagem1 += "/"+estado+".";
+
+console.log(mensagem1);
+
 
 // TEMPLATE STRING/LITERAL
+let mensagem2 = 
+`Meu nome é ${nome} ${sobrenome}, tenho ${idade} anos e moro em ${cidade}/${estado}. Sou ${idade >= 18 ? 'maior' : 'menor'} de idade.`;
 
-
-console.log(mensagem1)
+console.log(mensagem2);
