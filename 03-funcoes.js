730b36a1b6e8648bb08ddbb9f7640932d6360e26
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-06 09:22:49 -0300

    Cria arquivo funções

---
+++ 03-funcoes.js
@@ -0,0 +1,33 @@
+// 03-funcoes.js
+'use strict';
+
+exemplo1(); // chamada da função
+
+/* Sintaxe tradicional, função nomeada 
+A ordem de declaração da função neste caso NÃO IMPORTA */
+function exemplo1(){
+    console.log("Função nomeada");
+}
+
+exemplo1(); // chamada da função
+
+
+function separador(){
+    console.log("\n********************************\n");
+}
+
+separador();
+
+// Chamada aqui dá erro, pois não pode ser feita antes da declaração
+// exemplo2(); 
+
+/* Sintaxe tradicional, função anônima
+Nesta sintaxe, a ordem de declaração IMPORTA. */
+const exemplo2 = function(){
+    console.log("Função anônima");
+};
+
+exemplo2(); // chamada (sempre após declarar a função anônima)
+
+
+separador();
