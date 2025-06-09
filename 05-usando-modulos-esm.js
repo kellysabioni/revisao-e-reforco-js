dadd03aea0cee4c9caef9d70d5c3cd55fc4dc12c
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-09 10:32:03 -0300

    Atualiza com módulos dados e usa ALIAS/APELIDO

--- 05-usando-modulos-esm.js
+++ 05-usando-modulos-esm.js
@@ -5,6 +5,9 @@ import alunos from './modulos-esm/alunos.js'
 
 import { saudacao, clientes, separador } from './modulos-esm/exemplos.js';
 
+// Uso de um 'alias/apelido' para evitar conflito de nomes
+import { pessoa, livro, alunos as personagens } from "./modulos-esm/dados.js";
+
 saudacao("Fulano de tal");
 console.log(clientes);
 
@@ -15,3 +18,11 @@ console.log(alunos);
 for (const aluno of alunos) {
     console.log(aluno);
 }
+
+separador();
+
+// Acessando recursos do módulo 'dados.js'
+console.log(pessoa.nome);
+console.log(livro);
+console.log(personagens); // lembre-se: personagens é um apelido para alunos
+
