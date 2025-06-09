aa7b38ffb31fc57873dc3e4be504e89289966c8b
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-09 09:22:13 -0300

    Cria 05-usando usando modulos ESM

---
+++ 05-usando-modulos-esm.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Importação de múltiplos recursos/módulos usando ESM
+/*  Modulos ESM: EcmaScript Modules (sistema atual, moderno) */
+
+import { saudacao, clientes } from './modulos-esm/exemplos.js';
+
+saudacao("Fulano de tal");
+console.log(clientes);
