c4631a81bd6abb1d2dbf5f256b84af8441d60645
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-09 10:31:23 -0300

    Cria dados.js

---
+++ modulos-esm/dados.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const pessoa = {
+    nome: 'Ian Gillan',
+    idade: 78,
+    cidade: 'São Paulo'
+};
+
+const livro = {
+    titulo: 'O Senhor do anéis',
+    volume: 'As Duas Torres',
+    ano: 1954
+};
+
+const alunos = ['Pica Pau', 'Zé Colmeia', 'Naruto', 'Luffy'];
+
+export { pessoa, livro, alunos }
