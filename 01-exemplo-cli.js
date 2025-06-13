0431dd2a05001c4a75746ebe501759d781ef0ff2
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-13 09:48:33 -0300

    Atualiza exemplo 01

--- 01-exemplo-cli.js
+++ 01-exemplo-cli.js
@@ -1,13 +1,16 @@
-// Habilitando o modo estrito da linguagem 
-'use strict';
+// Habilitando o modo estrito da linguagem
+"use strict";
 
 let preco = 100;
 let resultado;
 
-if (preco >= 50) {
-    resultado = "Beleza";
+const aluno = "Fulano";
+console.log(aluno);
+
+if(preco >= 50){
+    resultado = "Beleza...";
 } else {
-    resultado = "Que droga";
+    resultado = "Que droga...";
 }
 
 console.log(resultado);
