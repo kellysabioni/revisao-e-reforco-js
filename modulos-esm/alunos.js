4691e765638b5cadd3eaaa8cd12a367bd9fb813c
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-13 09:57:05 -0300

    Atualiza alunos.js

--- modulos-esm/alunos.js
+++ modulos-esm/alunos.js
@@ -2,5 +2,5 @@
 
 const alunos = ["Ozzy", "Dio", "David", "Eddie", "Jon"];
 
-// Exportando como default (padrão), útil para recurso único(não precisa de objeto)
+// Exportando como default (padrão), útil para recurso único (não precisa de objeto)
 export default alunos;
