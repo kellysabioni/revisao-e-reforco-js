23b1ea854408ddd145f95b247699f28b827bd3fb
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-06-13 09:58:06 -0300

    Atualiza funcoes.js

--- modulos-esm/funcoes.js
+++ modulos-esm/funcoes.js
@@ -1,6 +1,23 @@
-const converterMaiuscula = (nome) => nome.toUpperCase();
-const converterMinuscula = (nome) => nome.toLowerCase();
-const formatarReal = (valor) =>
-  valor.toLocaleString("pt-br", { style: "currency", currency: "BRL" });
+'use strict';
 
-export { converterMaiuscula, converterMinuscula, formatarReal };
+export const converterMaiuscula = texto => texto.toUpperCase();
+
+export const converterMinuscula = texto => texto.toLowerCase();
+
+export const formatarMoeda = (valor, moeda = 'BRL', localidade = 'pt-BR' ) => {
+    return valor.toLocaleString(
+        localidade,
+        {
+            style: "currency",
+            currency: moeda
+        }
+    )
+};
+
+/* Usamos o rest operator para obter uma lista de parâmetros, não 
+importando a quantidade */
+export function classificar(...texto){
+    return texto.sort();
+}
+
+// export { converterMaiuscula, converterMinuscula, formatarMoeda }
